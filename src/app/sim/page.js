b4c9638d99c91474fe3615de67773d2df86e444a
Ayush_developer <2023csb1108@iitrpr.ac.in>
2025-03-23 03:31:42 +0530

    most of the issues are fixed, one issue, remains to read json file, hojaega jldi hi

--- src/app/sim/page.js
+++ src/app/sim/page.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import RegisterFile from "../components/registers";
 import MemoryFile from "../components/memory";
-import execution_output from "/app/src/executables";
+import execution_output from "../../executables";
 
 const VenusEditor = () => {
     const [activeTab, setActiveTab] = useState("Editor");
